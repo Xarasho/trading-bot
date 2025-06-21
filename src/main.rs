aafe564f3832338bd365e12ef9d415ef799d654e
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-21 15:07:07 -0600

    Added the Portfolio struct and implemented it with main_portfolio

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use dotenv::dotenv;
 use reqwest::Error;
-use std::env;
+use std::{collections::HashMap, env};
+
+#[derive(Debug)]
+struct Portfolio {
+    balance: f32,
+    assets: HashMap<String, i32>,
+}
 
 #[tokio::main]
-async fn get_stock_price() -> Result<f32, Error> {
-    dotenv().ok();
-    let api_key = match env::var("API_KEY") {
-        Ok(key) => key,
-        Err(_) => {
-            eprintln!("Error: API_KEY environment variable not found!");
-            std::process::exit(1);
-        }
-    };
+async fn get_current_stock_price(api_key: String, ticker: &str) -> Result<f32, Error> {
+    
 
-    let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=TSLA&apikey="
+    let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="
         .to_owned()
+        + ticker
+        + "&apikey="
         + &api_key;
 
     let response = reqwest::get(url).await?.json::<serde_json::Value>().await?;
@@ -34,6 +35,37 @@ async fn get_stock_price() -> Result<f32, Error> {
 }
 
 fn main() {
-    let x = get_stock_price().unwrap();
-    println!("STOCK: {}, PRICE: ${:?}", "TSLA", x);
+    // Reading API KEY
+    dotenv().ok();
+    let api_key = match env::var("API_KEY") {
+        Ok(key) => key,
+        Err(_) => {
+            eprintln!("Error: API_KEY environment variable not found!");
+            std::process::exit(1);
+        }
+    };
+
+    
+    // let ticker = "AAPL";
+    // let x = get_current_stock_price(api_key, ticker).unwrap();
+    // println!("STOCK: {}, PRICE: ${:?}", ticker, x);
+
+    let mut main_portfolio = Portfolio {
+        balance: 0.0,
+        assets: HashMap::new(),
+    };
+
+    let ticker = "AAPL";
+    let price: f32 = 201.39;
+    let shares: f32 = 5.0;
+
+    let cost_to_buy_x_shares: f32 = price * shares;
+    println!("STOCK: {}, PRICE: ${:?}", ticker, price);
+    println!("STOCK: {}, PRICE: ${:?}, Cost to buy {} shares = {}", 
+    ticker, price, shares, cost_to_buy_x_shares);
+    
+    dbg!(&main_portfolio);
+
+    main_portfolio.balance = 100.0;
+    dbg!(&main_portfolio);
 }
