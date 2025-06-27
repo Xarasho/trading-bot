5d3a5bde56d13dbe0e0bb4b8a5cc05ebc4399991
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-27 12:38:07 -0600

    Added alternative api call from finnhub

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use dotenv::dotenv;
 use reqwest::Error;
+use std::{thread, time};
 use std::{collections::HashMap, env};
 
 #[derive(Debug)]
@@ -8,11 +9,56 @@ struct Portfolio {
     assets: HashMap<String, f32>,
 }
 
+#[derive(Debug)]
+struct Trade_Position {
+    ticker: String,
+    open_price: f32,
+    close_price: f32,
+    // time_open: ?, 
+    // time_close: ?,
+}
+
+#[tokio::main]
+async fn finnhub_get_current_stock_price(ticker: &str) -> Result<f32, Error> {
+    // Reading API KEY
+    dotenv().ok();
+    let api_key = match env::var("FINHUB_API_KEY") {
+        Ok(key) => key,
+        Err(_) => {
+            eprintln!("Error: API_KEY environment variable not found!");
+            std::process::exit(1);
+        }
+    };
+
+    let url = "https://finnhub.io/api/v1/quote?symbol="
+        .to_owned()
+        + ticker
+        + "&token="
+        + &api_key;
+
+    let response = reqwest::get(url).await?.json::<serde_json::Value>().await?;
+    // println!("{:#?}", response["c"]);
+    let price = response["c"].to_string();
+    //let float_price: f32 = price.parse().unwrap();
+    let temp: Vec<char> = price.chars().collect();
+
+    // temp.remove(0);
+    // temp.pop();
+
+    let temp_string: String = temp.iter().collect();
+    let temp_f32: f32 = temp_string.parse().expect("Failed to parse f32");  
+
+    dbg!(temp_f32);
+    Ok(temp_f32)
+    
+}
+
+// DEAD
 #[tokio::main]
 async fn get_current_stock_price(ticker: &str) -> Result<f32, Error> {
     // Reading API KEY
     dotenv().ok();
-    let api_key = match env::var("API_KEY") {
+    let api_key = match env::var("ALPHA_API_KEY") {
         Ok(key) => key,
         Err(_) => {
             eprintln!("Error: API_KEY environment variable not found!");
@@ -49,7 +95,8 @@ fn calculate_portfolio_worth(portfolio: Portfolio) -> f32 {
         
         let ticker = stock.0;
         let amount_shares = stock.1;
-        let price_per_share = get_current_stock_price(ticker.as_str());
+        // let price_per_share = get_current_stock_price(ticker.as_str());
+        let price_per_share = finnhub_get_current_stock_price(ticker.as_str());
 
         match price_per_share {
             Ok(value) => {
@@ -111,31 +158,51 @@ fn main() {
         assets: HashMap::new(),
     };
 
-    //let cost_to_buy_x_shares: f32 = price * shares;
-    //println!("STOCK: {}, PRICE: ${:?}", ticker, price);
-    //println!("STOCK: {}, PRICE: ${:?}, Cost to buy {} shares = {}", 
-    //ticker, price, shares, cost_to_buy_x_shares);
+    // let cost_to_buy_x_shares: f32 = price * shares;
+    // println!("STOCK: {}, PRICE: ${:?}", ticker, price);
+    // println!("STOCK: {}, PRICE: ${:?}, Cost to buy {} shares = {}", 
+    // ticker, price, shares, cost_to_buy_x_shares);
     
-    //main_portfolio.cash_balance = 100.0;
+    // main_portfolio.cash_balance = 100.0;
     main_portfolio = update_cash_balance(main_portfolio, 100.0);
     main_portfolio = update_cash_balance(main_portfolio, -120.0);
     main_portfolio = add_stock_to_portfolio(main_portfolio, "AAPL".to_string(), 2.0);
     main_portfolio = remove_stock_from_portfolio(main_portfolio, "AAPL".to_string());
-    main_portfolio = add_stock_to_portfolio(main_portfolio, "MSFT".to_string(), 2.0);
-    main_portfolio = add_stock_to_portfolio(main_portfolio, "TSLA".to_string(), 2.0);
+    main_portfolio = add_stock_to_portfolio(main_portfolio, "MSFT".to_string(), 1.0);
     main_portfolio = add_stock_to_portfolio(main_portfolio, "GOOGL".to_string(), 2.0);
+    main_portfolio = update_stock_position(main_portfolio, "GOOGL".to_string(), -2.0);
+    main_portfolio = add_stock_to_portfolio(main_portfolio, "TSLA".to_string(), 2.0);
     main_portfolio = add_stock_to_portfolio(main_portfolio, "AMZN".to_string(), 2.0);
-    //main_portfolio.assets.insert("AAPL".to_string(), 2.0);
-    //main_portfolio.assets.insert("MSFT".to_string(), 1.5);
-    //main_portfolio.assets.insert("GOOGL".to_string(), 3.0);
-    //main_portfolio.assets.insert("AMZN".to_string(), 2.5);
-    //main_portfolio.assets.insert("TSLA".to_string(), 2.0);
-    main_portfolio = update_stock_position(main_portfolio, "AMZN".to_string(), -1.5);
-    dbg!(&main_portfolio);
-
-    // let x = calculate_portfolio_worth(main_portfolio);
-    // println!("Portfolio worth: ${}", x);
+    main_portfolio = update_stock_position(main_portfolio, "GOOGL".to_string(), -1.0);
+    // main_portfolio.assets.insert("AAPL".to_string(), 2.0);
+    // main_portfolio.assets.insert("MSFT".to_string(), 1.5);
+    // main_portfolio.assets.insert("GOOGL".to_string(), 3.0);
+    // main_portfolio.assets.insert("AMZN".to_string(), 2.5);
+    // main_portfolio.assets.insert("TSLA".to_string(), 2.0);
     
+    dbg!(&main_portfolio);
+    // dbg!(calculate_portfolio_worth(main_portfolio));
+
+    let temp = Trade_Position {
+        ticker: "AAPL".to_string(),
+        open_price: -100000000.0,
+        close_price: -100000000.0,
+    };
+    dbg!(temp);
+
+    //let x = calculate_portfolio_worth(main_portfolio);
+    // println!("Portfolio worth: ${}", x);
+
+    // loop {
+    //     let second = time::Duration::from_millis(1000);
+    //     thread::sleep(second);
+        
+    //     let ticker = "TSLA";
+    //     let x = finnhub_get_current_stock_price(ticker);
+        
+    //     println!("{}: ${:?}", ticker, x);
+    // }
+
     // let ticker = "AAPL";
     // let price: f32 = 201.39;
     // let shares: f32 = 5.0;
